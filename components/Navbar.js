658991c403cfa2c323f2d2e763950f6a83bc6595
LucasAndria <lucasandria01@gmail.com>
2022-07-11 08:35:42 +0300

    Navbar UI

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,21 @@
 import Image from "next/image"
 import Link from "next/link"
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 function Navbar({ lang }) {
-    const link = lang === 'fr' ? (
+    const link = (lang === 'fr') ? (
         { Home: 'Accueil', About: 'A propos', Skills: 'Compétences', Projects: 'Projets', Resume: 'Résumé', Contact: 'Contact' }
     ) : (
         { Home: 'Home', About: 'About', Skills: 'Skills', Projects: 'Projects', Resume: 'Resume', Contact: 'Contact' }
     )
     return (
-        <div className="fixed w-full h-20 shadow-xl z-[100]">
-            <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16 mx-5'>
-                <Image src='/../public/assets/lux-head.png' width='70' height='50' alt />
+        <div className="fixed ml-[1%] w-[98%] h-20 shadow-xl z-[100]">
+            <div className='flex justify-between items-center w-full h-full px-1 sm:px-5'>
+                <Image src='/assets/lux-head.png' width='70' height='50' alt />
                 <div>
-                    <ul className='hidden lg:flex mx-4 lg:mx-8'>
+                    <ul className='hidden lg:flex'>
                         <li className='ml-10 text-sm uppercase hover:border-b'>
                             <Link href='/'>{link.Home}</Link>
                         </li>
@@ -36,7 +38,7 @@ function Navbar({ lang }) {
                     <div
                         // style={{ color: `${linkColor}` }}
                         // onClick={handleNav}
-                        className='lg:hidden mr-8'
+                        className='lg:hidden'
                     >
                         <AiOutlineMenu size={25} />
                     </div>
@@ -44,19 +46,63 @@ function Navbar({ lang }) {
             </div>
             <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
                 <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3]">
-                    <div>
-                        <div className="flex mx-5 my-2 items-center justify-between">
-                            <Image src='/../public/assets/lux-head.png' width='60' height='35' alt />
+                    <div className="ml-[2%] w-[96%]">
+                        <div className="flex my-2 items-center justify-between">
+                            <Image src='/assets/lux-head.png' width='60' height='35' alt />
                             <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                                 <AiOutlineClose />
                             </div>
                         </div>
-                        <div>
-                            <p>{lang === 'fr' ? (
-                                'Blablabla'
-                            ) : (
-                                'eng eng eng eng '
-                            )}</p>
+                        <div className="border-b border-gray-300 my-4">
+                            <p className="w-[85%] md:w-[90%] py-4">
+                                {lang === 'fr' ? (
+                                    'Bienvenue !'
+                                ) : (
+                                    'Welcome to my portefolio !'
+                                )}</p>
+                        </div>
+                    </div>
+                    <div className="ml-[2%] w-[96%] py-4 flex flex-col">
+                        <ul className="uppercase">
+                            <Link href='/'>
+                                <li className="py-4 text-sm" >Home</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className="py-4 text-sm" >About</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className="py-4 text-sm" >Skills</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className="py-4 text-sm" >Projects</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className="py-4 text-sm" >Contact</li>
+                            </Link>
+                        </ul>
+                        <div className="pt-40">
+                            <p className="uppercase tracking-widest text-[#5651e5]">
+                                {lang === 'fr' ? (
+                                    'Me contacter'
+                                ) : (
+                                    'Get in touch with me'
+                                )}
+                            </p>
+                            <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <FaLinkedinIn />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <FaGithub />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <AiOutlineMail />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <BsFillPersonLinesFill />
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div>
       <Head>
         <title>{title}</title>
-        <link ref='icon' href="/vercel.svg" />
+        <link ref='icon' href="/assets/lux-head.png" />
       </Head>
 
       <Navbar lang={lang} />
