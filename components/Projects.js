6f567c967ccf30a7324ae3ac9a9ef97c7fb91728
LucasAndria <lucasandria01@gmail.com>
2022-08-25 13:58:43 +0300

    haha

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
 import ProjectItem from "./ProjectItem"
-// import bg_egouter from '../public/assets/Projects/e-gouter/e-gouter.jpg'
-// import bg_todolist from '../public/assets/Projects/todo-list/capture.png'
-// import bg_rescues from '../public/assets/Projects/the-rescues/bg-1.png'
+import bg_egouter from "./assets/Projects/e-gouter/e-gouter.jpg"
+import bg_todolist from "./assets/Projects/todo-list/capture.png"
+import bg_rescues from "./assets/Projects/the-rescues/bg-1.png"
 
 function Projects() {
 
@@ -11,9 +11,9 @@ function Projects() {
                 <p className="text-xl tracking-widest uppercase text-[#5651e5]">Projets</p>
                 <h2 className="py-4">{"Ce que j'ai créer"}</h2>
                 <div className="grid md:grid-cols-2 gap-8">
-                    {/* <ProjectItem titre='E-Gouter-Za' descri='Une application mobile qui permet de faire des commandes.' bgImg={bg_egouter} url={'/projects/e-gouter'} />
+                    <ProjectItem titre='E-Gouter-Za' descri='Une application mobile qui permet de faire des commandes.' bgImg={bg_egouter} url={'/projects/e-gouter'} />
                     <ProjectItem titre='The rescues' descri="Un jeu platformer que l'on a créer en 24h pour la gamejam+ 2020" bgImg={bg_rescues} url={'/projects/the-rescues'} />
-                    <ProjectItem titre='Todo List' descri="Une application web pour mémoriser les taches à accomplir" bgImg={bg_todolist} url={'/projects/todo-list'} /> */}
+                    <ProjectItem titre='Todo List' descri="Une application web pour mémoriser les taches à accomplir" bgImg={bg_todolist} url={'/projects/todo-list'} />
                 </div>
             </div>
         </div>
