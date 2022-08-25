e3756dbc86619c7aec2ce4d37e2e8e137d0a7bd7
LucasAndria <lucasandria01@gmail.com>
2022-08-25 13:25:28 +0300

    dels string ref

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Lucas | Développeur junior</title>
-        <link ref='icon' href="/assets/lux-head.png" />
+        <link ref={'icon'} href="/assets/lux-head.png" />
       </Head>
       <Main />
       <About />
