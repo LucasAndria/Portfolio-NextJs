5128f61c8f81f364d8d040545ea0fbecb435336d
LucasAndria <lucasandria01@gmail.com>
2022-09-04 18:45:38 +0300

    add the training timer

--- components/Projects.js
+++ components/Projects.js
@@ -14,6 +14,7 @@ function Projects() {
                     <ProjectItem titre='E-Gouter-Za' descri='Une application mobile qui permet de faire des commandes.' bgImg={bg_egouter} url='/projects/e-gouter' />
                     <ProjectItem titre='The rescues' descri="Un jeu platformer que l'on a créer en 24h pour la gamejam+ 2020" bgImg={bg_rescues} url='/projects/the-rescues' />
                     <ProjectItem titre='Todo List' descri="Une application web pour mémoriser les taches à accomplir" bgImg={bg_todolist} url='/projects/todo-list' />
+                    <ProjectItem titre='Training timer' descri="Une application mobile utiliser pendant un entrainement" bgImg={bg_todolist} url='/projects/training-timer' />
                 </div>
             </div>
         </div>
