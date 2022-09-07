4d3d7123e29c3aa8516f2b86fbd6e5d2f41a1bec
LucasAndria <lucasandria01@gmail.com>
2022-09-07 17:09:55 +0300

    style for the copy button

--- components/Contact.js
+++ components/Contact.js
@@ -5,12 +5,14 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa"
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 import { CVButton, GithubButton, LinkedInButton, MailButton } from "./links/Button"
 import emailjs from '@emailjs/browser';
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import Image from "next/image"
 import ContactImg from '../public//assets/Contacter.png'
 
 function Contact() {
 
+    const [copyMailLoaded, setCopyMailLoaded] = useState(false)
+    const [copyNumLoaded, setCopyNumLoaded] = useState(false)
     const form = useRef();
 
     function sendEmail(e) {
@@ -26,6 +28,23 @@ function Contact() {
 
         e.target.reset()
     }
+
+    function copyMail() {
+        navigator.clipboard.writeText("lucasandria01@gmail.com")
+        setCopyMailLoaded(true)
+        setTimeout(() => {
+            setCopyMailLoaded(false)
+        }, 800);
+    }
+
+    function copyNum() {
+        navigator.clipboard.writeText("034 20 529 88")
+        setCopyNumLoaded(true)
+        setTimeout(() => {
+            setCopyNumLoaded(false)
+        }, 800);
+    }
+
     return (
         <div id='contact' className="w-full lg:h-screen">
             <div className="max-w-[1240px] m-auto px-2 py-24 w-full">
@@ -44,8 +63,38 @@ function Contact() {
                             </div>
                             <div className="mt-5">
                                 {/* <p className="uppercase pt-8">Lien : </p> */}
-                                <p className="py-2 font-bold">lucasandria01@gmail.com <button className="ml-2 px-2 bg-blue-600 rounded-xl" onClick={() => (navigator.clipboard.writeText("lucasandria01@gmail.com"), alert('email copié'))}>copier</button></p>
-                                <p className="py-2 font-bold">034 20 529 88 <button className="ml-2 px-2 bg-blue-600" onClick={() => (navigator.clipboard.writeText("034 20 529 88"), alert('numéro copié'))}>copier</button></p>
+                                <p className="py-2 font-bold flex gap-3">lucasandria01@gmail.com
+                                    <button
+                                        className="px-2 py-1 text-sm font-medium rounded text-white bg-blue-600 hover:bg-blue-600/80"
+                                        onClick={copyMail}
+                                    >
+                                        <svg className={copyMailLoaded ? "w-4 h-4 text-white animate-ping absolute text-center" : "hidden"} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                                        </svg>
+                                        <span className={copyMailLoaded ? "invisible" : ""}>
+                                            <svg className="w-4 h-4 text-white text-center" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                                            </svg>
+                                        </span>
+                                    </button>
+                                </p>
+                                <p className="py-2 font-bold  flex gap-3">034 20 529 88
+                                    <button
+                                        className="px-2 py-1 text-sm font-medium rounded text-white bg-blue-600 hover:bg-blue-600/80"
+                                        onClick={copyNum}
+                                    >
+                                        <svg className={copyNumLoaded ? "w-4 h-4 text-white animate-ping absolute text-center" : "hidden"} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                                        </svg>
+                                        <span className={copyNumLoaded ? "invisible" : ""}>
+                                            <svg className="w-4 h-4 text-white text-center" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                                            </svg>
+                                        </span>
+                                    </button>
+                                </p>
 
                                 <div className="flex items-center justify-between py-4">
                                     <LinkedInButton />
