83ebea6b369680b362a7962d49c77f90389d9d39
LucasAndria <lucasandria01@gmail.com>
2022-09-10 11:27:07 +0300

    asset for the training timer

--- components/Projects.js
+++ components/Projects.js
@@ -3,6 +3,7 @@ import bg_egouter from "../public/assets/Projects/e-gouter/gouter.jpg"
 import bg_todolist from "../public/assets/Projects/todo-list/capture.png"
 import bg_rescues from "../public/assets/Projects/the-rescues/bg-1.png"
 import bg_multiTimer from "../public/assets/Projects/multi-timer/bg.png"
+import bg_trainingTimer from "../public/assets/Projects/training-timer/bg.png"
 
 function Projects() {
 
@@ -15,7 +16,7 @@ function Projects() {
                     <ProjectItem titre='E-Gouter-Za' descri='Une application mobile qui permet de faire des commandes.' bgImg={bg_egouter} url='/projects/e-gouter' />
                     <ProjectItem titre='The rescues' descri="Un jeu platformer que l'on a créer en 24h pour la gamejam+ 2020" bgImg={bg_rescues} url='/projects/the-rescues' />
                     <ProjectItem titre='Todo List' descri="Une application web pour mémoriser les taches à accomplir" bgImg={bg_todolist} url='/projects/todo-list' />
-                    <ProjectItem titre='Training timer' descri="Une application mobile utiliser pendant un entrainement" bgImg={bg_todolist} url='/projects/training-timer' />
+                    <ProjectItem titre='Training timer' descri="Une application mobile utiliser pendant un entrainement" bgImg={bg_trainingTimer} url='/projects/training-timer' />
                     <ProjectItem titre='Multi Timer' descri="Un logiciel utiliser pour faire la décompte de chaque postes dans un salle de jeu" bgImg={bg_multiTimer} url='/projects/multi-timer' />
                 </div>
             </div>
