960cab414a49a7fe2db5fee498b341b96a67c66d
LucasAndria <lucasandria01@gmail.com>
2022-09-21 18:49:16 +0300

    organisation des priorité des projets

--- components/Projects.js
+++ components/Projects.js
@@ -13,11 +13,11 @@ function Projects() {
                 <p className="text-xl tracking-widest uppercase text-[#5651e5]">Projets</p>
                 <h2 className="py-4">{"Ce que j'ai créé"}</h2>
                 <div className="grid md:grid-cols-2 gap-8">
-                    <ProjectItem titre='E-Gouter-Za' descri='Une application mobile qui permet de faire des commandes.' bgImg={bg_egouter} url='/projects/e-gouter' />
-                    <ProjectItem titre='The rescues' descri="Un jeu platformer que l'on a créer en 24h pour la gamejam+ 2020" bgImg={bg_rescues} url='/projects/the-rescues' />
                     <ProjectItem titre='Todo List' descri="Une application web pour mémoriser les taches à accomplir" bgImg={bg_todolist} url='/projects/todo-list' />
                     <ProjectItem titre='Training timer' descri="Une application mobile utiliser pendant un entrainement" bgImg={bg_trainingTimer} url='/projects/training-timer' />
                     <ProjectItem titre='Multi Timer' descri="Un logiciel utiliser pour faire la décompte de chaque postes dans un salle de jeu" bgImg={bg_multiTimer} url='/projects/multi-timer' />
+                    <ProjectItem titre='E-Gouter-Za' descri='Une application mobile qui permet de faire des commandes.' bgImg={bg_egouter} url='/projects/e-gouter' />
+                    <ProjectItem titre='The rescues' descri="Un jeu platformer que l'on a créer en 24h pour la gamejam+ 2020" bgImg={bg_rescues} url='/projects/the-rescues' />
                 </div>
             </div>
         </div>
