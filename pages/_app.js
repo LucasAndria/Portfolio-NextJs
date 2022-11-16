cf98a5d7163e2a8cb4fcf5e75150fed7d506b9db
LucasAndria <lucasandria01@gmail.com>
2022-11-16 13:34:22 +0300

    FIX: About me

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import Navbar from '../components/Navbar';
-import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/react";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
